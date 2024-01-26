10b31f2a6e10e712c662c7a39494f3ed4745c749
dCOMMICS <sanwurld@gmail.com>
2024-01-26 16:54:37 +0300

    scene failed to load

---
+++ js/index.js
@@ -0,0 +1,40 @@
+import * as THREE from 'three';
+
+let camera, scene, render;
+let geometry, material, mesh;
+
+init() {
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+    camera.position.z = 1;
+
+    scene = new THREE.Scene();
+
+
+    geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+    material = new THREE.MeshNormalMaterial();
+
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(width, height);
+    renderer.setAnimationLoop(animation);
+    let container = document.getElementById('container');
+    container.appendChild(renderer.domElement);
+
+}
+
+
+
+
+// animation
+
+function animation(time) {
+
+    mesh.rotation.x = time / 2000;
+    mesh.rotation.y = time / 1000;
+
+    renderer.render(scene, camera);
+
+}
+
